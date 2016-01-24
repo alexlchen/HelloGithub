3bfd91a100ff4c5df1aee6d4debb21b567aeab11
alexlchen <alex.liyi.chen@gmail.com>
2016-01-24 00:50:29 -0500

    Changed main.js
    Add a new class Person to main.js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+var Person = (function () {
+    function Person(name) {
+        this.name = name;
+    }
+    Person.prototype.talk = function () {
+        return "Bra, bra, bra...";
+    };
+    return Person;
+})();
+
 var Greeter = (function () {
     function Greeter(message) {
         this.greeting = message;
@@ -8,5 +18,7 @@ var Greeter = (function () {
     return Greeter;
 })();
 
-var greeter = new Greeter(" GitHub. I am here now.");
+var alex = new Person("Alex");
+console.log(alex.talk());
+var greeter = new Greeter(" GitHub.");
 console.log(greeter.greet());
