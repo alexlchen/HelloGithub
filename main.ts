b6e1948879c02d93e538f83b03a0cfd89b223a7d
alexlchen <alex.liyi.chen@gmail.com>
2016-01-23 22:55:01 -0500

    Works in January 23 2016
    I added main.js and index.html files to the project.

---
+++ main.ts
@@ -0,0 +1,15 @@
+class Greeter{
+    
+    greeting:string;
+    
+    constructor(message:string){
+        this.greeting = message;
+    }
+    
+    greet(){
+        return "Hello, " + this.greeting;
+    }
+}
+
+var greeter = new Greeter(" GitHub.");
+console.log(greeter.greet());
